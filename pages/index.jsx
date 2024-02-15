a4ed5286a0d2afd5a574f177efa93c4eb064154e
fuji <funnybunny6190@gmail.com>
2024-02-15 18:11:25 +0900

    試しにclickイベントを書いてみました

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,16 +2,26 @@ import Head from "next/head";
 import { Inter } from "next/font/google";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
+import { useCallback } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const handleClick = useCallback((e) => {
+    console.log(e.target.href);
+    e.preventDefault();
+    alert(123);
+  }, []);
+
   return (
     <>
       <Head>
         <title>indes page</title>
       </Head>
       <Header />
+      <a href="/about" onClick={handleClick}>
+        ボタン
+      </a>
       <Main page="index" />
     </>
   );
