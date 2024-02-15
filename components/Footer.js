a9128583a7fe52c9cc0c177685829fe9ba8296e7
fuji <funnybunny6190@gmail.com>
2024-02-15 16:23:04 +0900

    Linksのリファクタリング

--- components/Footer.js
+++ components/Footer.js
@@ -3,35 +3,40 @@ import styles from "./Footer.module.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
+const ITEMS = [
+  {
+    href: "https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Templates",
+    description:
+      "Discover and deploy boilerplate example Next.js&nbsp;projects.",
+  },
+  {
+    href: "https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",
+    title: "Deploy",
+    description:
+      "Instantly deploy your Next.js site to a shareable URL with&nbsp;Vercel.",
+  },
+];
+
 export function Footer() {
   return (
     <div>
-      <a
-        href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>
-          Templates <span>-&gt;</span>
-        </h2>
-        <p>Discover and deploy boilerplate example Next.js&nbsp;projects.</p>
-      </a>
-
-      <a
-        href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-        className={styles.card}
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <h2>
-          Deploy <span>-&gt;</span>
-        </h2>
-        <p>
-          Instantly deploy your Next.js site to a shareable URL
-          with&nbsp;Vercel.
-        </p>
-      </a>
+      {ITEMS.map((item) => {
+        return (
+          <a
+            key={item.href}
+            href={item.href}
+            className={styles.card}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <h2>
+              {item.title} <span>-&gt;</span>
+            </h2>
+            <p>{item.description}</p>
+          </a>
+        );
+      })}
     </div>
   );
 }
