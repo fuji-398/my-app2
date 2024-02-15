a8030cdb4ee31029392bd1c4b692370a576f9cdd
fuji <funnybunny6190@gmail.com>
2024-02-15 15:55:41 +0900

    Headerの追加

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+import { Inter } from "next/font/google";
+import styles from "./Header.module.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export function Header() {
+  return (
+    <header className={styles.header}>
+      <Link href="/">
+        <p className={styles.anchor}>Index</p>
+      </Link>
+      <Link href="/about">
+        <p>About</p>
+      </Link>
+    </header>
+  );
+}
